01c300e6f2eab0aa4f213987326807fd520b7e97
„Maciej <poborcamaciej@gmail.com>
2022-05-24 10:28:55 +0200

    Hub and Spoke using vpn gateway

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "location" {}
+variable "resourcegroup_name" {
+  
+}
+variable "HubNetwork_Name" { 
+}
+variable "Spoke01Network_Name" {
+  
+}
+variable "Spoke02Network_Name" {
+  
+}
+variable "vpn_name" {
+  
+}
